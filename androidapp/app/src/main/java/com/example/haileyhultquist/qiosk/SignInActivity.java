d443f9bf9c5a68a5f8ee12d759c0291ff1e2b94b
Takondwa Kakusa <tk421@vt.edu>
2017-07-15 23:10:56 -0700

    nothing

--- androidapp/app/src/main/java/com/example/haileyhultquist/qiosk/SignInActivity.java
+++ androidapp/app/src/main/java/com/example/haileyhultquist/qiosk/SignInActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
